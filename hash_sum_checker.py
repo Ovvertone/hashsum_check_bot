8e638ffded8578b9839bb79ff097c61d5b6d24a3
Vyacheslav <shirkeev@example.com>
2020-11-04 23:06:33 +0700

    refactor

--- hash_sum_checker.py
+++ hash_sum_checker.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 from hashlib import md5
+from os.path import abspath
 import requests
 import sys
 
+FILENAME = abspath('data1.html')
 
-def parser(url: str) -> str:
+
+def parser(url):
     return str(BeautifulSoup(requests.get(url=url).content, 'html.parser'))
 
 
-def hash_sum(filename: str) -> str:
+def hash_sum(filename=FILENAME):
     hash_alg = md5()
     with open(filename, 'rb') as file:
         for chunk in iter(lambda: file.read(4096), b''):
@@ -17,6 +20,6 @@ def hash_sum(filename: str) -> str:
 
 
 if __name__ == '__main__':
-    with open('data.html', 'w') as file:
+    with open(FILENAME, 'w') as file:
         file.write(parser(sys.argv[1]))
-    print(hash_sum('data.html'))
+    print(hash_sum())
