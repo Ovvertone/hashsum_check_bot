31ff4b6fbd283f92b5c93047cd97b7be2910674f
Vyacheslav <shirkeev@example.com>
2020-11-05 17:15:25 +0700

    Minor refactor

--- hash_sum_checker.py
+++ hash_sum_checker.py
@@ -23,17 +23,19 @@ except ConfigurationError:
 
 def parser(url=URL):
     try:
-        return BeautifulSoup(requests.get(url).content, 'lxml')
+        soup = BeautifulSoup(requests.get(url).content, 'lxml')
     except ConnectionError:
         exit('Invalid url')
+    return [str(tag) for tag in soup('a')]
 
 
-def hash_sum():
+def hash_sum(data=parser()):
     hash_alg = md5()
-    for tag in parser():
+    for tag in data:
         hash_alg.update(tag.encode())
     return hash_alg.hexdigest()
 
 
 if __name__ == '__main__':
+    pass
     collection.find_one_and_update({'url': URL}, {'$set': {'hash_sum': hash_sum()}}, upsert=True)
